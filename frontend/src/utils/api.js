164bd7c0e92a02ab7e6efecb3fc0f0cdb641fa2c
jonorberias-blip <jonorberias@gmail.com>
2026-04-29 15:06:10 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://bibliotheque-qcs2.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
